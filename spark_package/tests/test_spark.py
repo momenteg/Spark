ff2697584ac08fecfdde5b93e3e4d840870bcba3
Giulio Momentè <momenteg@gmail.com>
2021-07-22 10:43:01 +0200

    Update test_spark.py
    Minor mostly cosmetic refactoring of unit tests

--- spark_package/tests/test_spark.py
+++ spark_package/tests/test_spark.py
@@ -4,14 +4,27 @@ import databricks.koalas as ks
 
 
 class TestSpark(unittest.TestCase):
-    def test_simple_dataframe(self):
-        dataset = [("abc", 1), ("def", 2)]
-        columns = ["name", "age"]
-        spark, sc = init_spark()
-        source_df = ks.DataFrame(data=dataset, columns=columns)
-        self.assertEqual(len(source_df), 2)
-        self.assertEqual(source_df.columns.tolist(), columns)
+    def setUp(self):
+        self.dataset = [("abc", 1), ("def", 2)]
+        self.columns = ["name", "age"]
+        self.df = ks.DataFrame(data=self.dataset, columns=self.columns)
+
+    def test_len_df(self):
+        self.assertEqual(len(self.df), 2)
+
+    def test_columns_df(self):
+        self.assertEqual(self.df.columns.tolist(), self.columns)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestSpark))
+    # or you can add specific function with the following:
+    # suite.addTest(TestSpark('test_len_df'))
+    # suite.addTest(TestSpark('test_columns_df'))
+    return suite
 
 
 if __name__ == "__main__":
-    unittest.main()
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
